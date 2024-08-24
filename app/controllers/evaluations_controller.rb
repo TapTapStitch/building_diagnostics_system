e68576e782a5c5096836f3987b58d094c2b22197
Mykyta Zub <mykyta_zub@epam.com>
2024-08-24 15:47:38 +0300

    fix

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
 class EvaluationsController < ApplicationController
-  before_action :set_defect
+  before_action :set_building
   before_action :set_evaluation, only: %i[update destroy]
 
   def create
-    @evaluation = @defect.evaluations.build(evaluation_params)
+    @evaluation = @building.evaluations.build(evaluation_params)
     if @evaluation.save
-      redirect_to building_url(@defect.building), notice: I18n.t('evaluations.create.success')
+      redirect_to @building, notice: I18n.t('evaluations.create.success')
     else
-      redirect_to building_url(@defect.building), alert: I18n.t('evaluations.create.failure')
+      redirect_to @building, alert: I18n.t('evaluations.create.failure')
     end
   end
 
   def update
     if @evaluation.update(evaluation_params)
-      redirect_to building_url(@defect.building), notice: I18n.t('evaluations.update.success')
+      redirect_to @building, notice: I18n.t('evaluations.update.success')
     else
-      redirect_to building_url(@defect.building), alert: I18n.t('evaluations.update.failure')
+      redirect_to @building, alert: I18n.t('evaluations.update.failure')
     end
   end
 
   def destroy
     @evaluation.destroy!
-    redirect_to building_url(@defect.building), notice: I18n.t('evaluations.destroy')
+    redirect_to @building, notice: I18n.t('evaluations.destroy')
   end
 
   private
 
-  def set_defect
-    @defect = Defect.find(params[:defect_id])
+  def set_building
+    @building = Building.find(params[:building_id])
   end
 
   def set_evaluation
-    @evaluation = @defect.evaluations.find(params[:id])
+    @evaluation = Evaluation.find(params[:id])
   end
 
   def evaluation_params
-    params.require(:evaluation).permit(:rating, :expert_id)
+    params.require(:evaluation).permit(:defect_id, :expert_id, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@
 Rails.application.routes.draw do
   root 'buildings#index'
   resources :buildings do
-    resources :defects, only: %i[create update destroy] do
-      resources :evaluations, only: %i[create update destroy]
-    end
+    resources :defects, only: %i[create update destroy]
     resources :experts, only: %i[create update destroy]
+    resources :evaluations, only: %i[create update destroy]
   end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
